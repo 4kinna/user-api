0736c30cebbce0c56243ce0388efa2ada7305e89
Annika <a.staalhandske@gmail.com>
2021-04-30 10:10:50 +0200

    adde connection to db, added feature to delete specific user

--- src/main/java/com/example/userapi/UserApiApplication.java
+++ src/main/java/com/example/userapi/UserApiApplication.java
@@ -1,13 +1,20 @@
 package com.example.userapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@ComponentScan
 @SpringBootApplication
+@PropertySource(value = "classpath:application.properties")
 public class UserApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserApiApplication.class, args);
-	}
+    public static void main(String... args) {
+        SpringApplication.run(UserApiApplication.class, args);
+    }
 
 }
