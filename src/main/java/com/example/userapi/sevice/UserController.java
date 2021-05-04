98f113aefbed4047a3f585dbbe4976fc520342d8
Annika <a.staalhandske@gmail.com>
2021-05-04 11:24:37 +0200

    modified some kod to complie with other data in UI

--- src/main/java/com/example/userapi/sevice/UserController.java
+++ src/main/java/com/example/userapi/sevice/UserController.java
@@ -13,37 +13,50 @@ public class UserController {
 
     /* @GetMapping ensures that HTTP Get requests to /users that are mapped to the users() method*/
     /*Get entire list of user*/
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/users")
     public List<Users> all() {
         return repository.findAll();
     }
 
     /*Get specific user depending on id */
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/users/{userid}")
     public Users getUserById(@PathVariable(value = "userid") Integer userid) {
         return repository.findById(userid).orElse(null);
     }
 
     /*Remove specific user*/
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/users/{userid}")
-    public void deleteUser(@PathVariable(value="userid") Integer userid){
+    public void deleteUser(@PathVariable(value = "userid") Integer userid) {
 
         repository.deleteById(userid);
     }
 
 
     /*Update a specific user depending on ID*/
-   @PutMapping("/users/{userid}")
-    public Optional<Users> updateUser(@PathVariable(value = "userid") Integer userid, @RequestBody Users userDetails) {
-       return repository.findById(userid).map(user ->{
-           user.setProfilePicture(userDetails.getProfilePicture());
-           user.setName(userDetails.getName());
-           user.setCompany(userDetails.getCompany());
-           user.setEmail(userDetails.getEmail());
-           user.setPhone(userDetails.getPhone());
-           return repository.save(user);
-       });
-
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PutMapping("/users/{userid}")
+    public void updateUser(@PathVariable(value = "userid") Integer userid, @RequestBody Users userDetails) {
+        System.out.println(userid);
+        repository.findById(userid).map(user -> {
+            user.setProfilePicture(userDetails.getProfilePicture());
+            user.setName(userDetails.getName());
+            user.setCompany(userDetails.getCompany());
+            user.setEmail(userDetails.getEmail());
+            user.setPhone(userDetails.getPhone());
+            return repository.save(user);
+        });
     }
+
+
+    /*Add new user*/
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/users")
+    public Users newUser(@RequestBody Users newUser) {
+        return repository.save(newUser);
+    }
+
 }
 
